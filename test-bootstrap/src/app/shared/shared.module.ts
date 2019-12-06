c13b8af1afb55799a7cbc3fb4e673fa0c2ee733f
Samir Imanov <imanovsamir@gmail.com>
2019-12-07 00:40:46 +0400

    added shared module

---
+++ test-bootstrap/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent, FooterComponent } from './layout';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CommonModule, HeaderComponent, FooterComponent
+  ]
+})
+export class SharedModule { }
