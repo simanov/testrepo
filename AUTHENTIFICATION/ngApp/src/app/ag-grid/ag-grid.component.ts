27f3c4731c9097464dca2408870b7255cd27b031
Samir Imanov <imanovsamir@gmail.com>
2019-12-20 22:37:27 +0400

    finished ag-grid

--- AUTHENTIFICATION/ngApp/src/app/ag-grid/ag-grid.component.ts
+++ AUTHENTIFICATION/ngApp/src/app/ag-grid/ag-grid.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-ag-grid',
@@ -6,22 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ag-grid.component.scss']
 })
 export class AgGridComponent implements OnInit {
+  @ViewChild('agGrid', { static: true }) agGrid: AgGridAngular;
 
   columnDefs = [
-    { headerName: 'Make', field: 'make', sortable: true },
-    { headerName: 'Model', field: 'model', sortable: true },
-    { headerName: 'Price', field: 'price', sortable: true }
+    { headerName: 'Make', field: 'make', sortable: true, filter: true, checkboxSelection: true },
+    { headerName: 'Model', field: 'model', sortable: true, filter: true },
+    { headerName: 'Price', field: 'price', sortable: true, filter: true }
   ];
 
+  /*
   rowData = [
     { make: 'Toyota', model: 'Celica', price: 35000 },
     { make: 'Ford', model: 'Mondeo', price: 32000 },
     { make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
+  ];*/
 
-  constructor() { }
+  rowData: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.rowData = this.http.get<any>('https://api.myjson.com/bins/ly7d1');
+  }
+
+  getSelectedRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map(node => node.data);
+    const selectedDataStringPresentation = selectedData.map(node => node.make + ' ' + node.model).join(', ');
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
   }
 
 }
